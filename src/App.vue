e75665ba7c90f0c3be53fcf50d5520a70db25ff9
JuicySoup <contactjonathanosterberg@gmail.com>
2021-10-16 10:47:23 +0200

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,82 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div class="nav-container">
+  <header>
+    <nav class="nav-container">
+      <ul>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    </header>
+    </div>
+    <body>
+      <main id="app" class="hero">
+        <div class="content">
+          <h1>Jonathan Österberg</h1>
+          <p>I am a .NET Developer student and self-taught 3D Artist from Sweden.</p>
+          <div class="actionbuttons">
+            <button class="portfolio-btn">View Portfolio</button>
+            <button class="skills-btn">My Skills</button>
+          </div>
+        </div>
+        <section class="portfolio">
+          <div class="inner transition2">
+            <p class="subtitle">&lt;Portfolio&gt;</p>
+            <h2 class="portfolio-title">Work I have done</h2>
+          </div>
+          <div class="portfolio-grid">
+            <div class="portfolio-menu">
+              <a href="#">Everything</a>
+              <a href="#">3D Animations</a>
+              <a href="#">3D renders</a>
+              <a href="#">videos</a>
+              <a href="#">web</a>
+            </div>
+            <div class="portfolio-items">
+              <PortfolioGrid
+                v-for="item in portfolioitems"
+                :key="item.title"
+                :item="item"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+  </body>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import PortfolioGrid from './components/PortfolioGrid.vue';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "app",
+  components: {
+    PortfolioGrid,
+  },
+  data() {
+    return {
+      portfolioitems: [
+        {
+          title: "Environment intro",
+          category: "3D Animation",
+          created: "Aug 2020",
+          src: require('./assets/Environment.png')
+        },
+        {
+          title: "Garen",
+          category: "3D Render",
+          created: "Aug 2021",
+          src: require('./assets/Garen.png')
+        },
+        {
+          title: "Hellfire",
+          category: "3D Animation",
+          created: "Oct 2021",
+          src: require('./assets/Hellfire_2k.png')
+        },
+      ],
+    }
+  }
+};
+</script>
---
+++ src/components/PortfolioGrid.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="item">
+    <div class="item-image-wrap">
+        <img :src="item.src" class="image">
+    </div>
+    <div class="item-inner">
+        <p class="date-text">{{item.created}}</p>
+        <div class="item-text-wrap">
+            <p>{{item.category}}</p>
+            <h3>{{item.title}}</h3>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'item',
+    props: ['item']
+}
+</script>
+
+<style>
+    .item{
+        flex: 1 1 33.333%;
+        position: relative;
+        margin-top: 3em;
+    }
+    .date-text{
+        position: absolute;
+        top: 0;
+        left: 0;
+        font-size: 0.6em;
+        padding: 0 1em;
+    }
+    .item-text-wrap{
+        position:absolute;
+        bottom: 0;
+        left: 0;
+        padding: .5em .6em;
+    }
+    .item-text-wrap > p{
+        color: #FF6868;
+        font-size: 0.6em;
+        text-align: left;
+        margin-bottom: -1em;
+
+    }
+    .item-text-wrap > h3{
+        font-size:0.7em;
+        font-weight: 600;
+        text-align: left;
+    }
+    
+
+
+</style>
