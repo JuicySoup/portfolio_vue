9506b06ad8d4b1cef668fb7fd58561fbb00b7be3
JuicySoup <contactjonathanosterberg@gmail.com>
2021-10-23 14:38:16 +0200

    Added project form

--- src/components/ProjectForm.vue
+++ src/components/ProjectForm.vue
@@ -41,30 +41,110 @@
                     <div class="col col-md-3">
                         <label for="file-input" class=" form-control-label">File input</label>
                     </div>
-                    <div class="col-12 col-md-9">
-                        <input type="file" id="file-input" name="file-input" class="form-control-file">
+                    <div class="col-12 col-md-9 form-file">
+                        <input type="file" id="file-input" name="file-input" class="form-control-file" @change="handleChange">
+                        <p v-if="fileError" class=" form-error-label">{{fileError}}</p>
+                        <p v-if="file" class=" form-control-label">{{file.name}}</p>
+                        <p v-else>Drag your file here or click in this area.</p>
                     </div>
-                </div>
-            </form>
-        </div>
-        <div class="card-footer">
+                    <div class="col col-md-3">
+
+                    </div>
+                            <div class="card-footer">
             <button type="submit" class="btn btn-primary btn-sm">
-                <i class="fa fa-dot-circle-o"></i> Submit
+                <i class="fa fa-dot-circle-o"></i> Create
             </button>
             <button type="reset" class="btn btn-danger btn-sm">
                 <i class="fa fa-ban"></i> Reset
             </button>
+        </div>
+                </div>
+            </form>
         </div>
     </div>
     </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+
 export default {
+    setup(){
+
+        const file = ref(null)
+        const fileError = ref(null)
+
+        // allowed file types
+
+        const types = ['image/png', 'image/jpeg']
+
+        const handleChange = (e) => {
+            let selected = e.target.files[0]
+            console.log(selected)
+
+            if(selected && types.includes(selected.type)){
+                file.value = selected
+                fileError.value = null
+            } else {
+                file.value = null
+                fileError.value = 'Select an image file (png or jpeg)'
+            }
+        }
+
+        return { handleChange, file, fileError }
+    }
 
 }
 </script>
 
-<style scoped src="../assets/styles/theme.css">
+<style >
+    form {
+        margin: 30px auto 10px;
+        text-align: center;
+    }
+    label{
+        display: block;
+        margin: 10px auto;
+        line-height: 30px;
+    }
+    textarea{
+        width: 300px;
+        height: 250px;
+    }
+    .form-file{
+        margin: auto;
+        width: 500px;
+        height: 200px;
+        border: 4px dashed #fff;
+    
+    }
 
+    .form-file input{
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        outline: none;
+        opacity: 0;
+    }
+
+    form p{
+        margin-top: -200px;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        line-height: 170px;
+        color: #ffffff;
+        font-family: Arial;
+    }
+
+    form button{
+        width: 150px;
+        height: 30px;
+        margin: 20px auto;
+        cursor: pointer;
+        background: #5ad8b2;
+        border: 1px solid #5ad8b2;
+        border-radius: 6px;
+    }
 </style>
